e95e0d8f07fbac6c4138135726ba093bf0e574b7
svsgvarma <svsgvarma@gmail.com>
2023-08-30 12:19:08 +0200

    commit1

--- 2.0_parse_polymarker_input.py
+++ 2.0_parse_polymarker_input.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  parse_polymarker_input.py
-#  
-#  Copyright 2017 Junli Zhang <junli@debian>
-#  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
@@ -21,6 +17,7 @@
 #  MA 02110-1301, USA.
 #  
 #  
+
 """
 Parse polymarker input and output a fasta file for blast.
 
